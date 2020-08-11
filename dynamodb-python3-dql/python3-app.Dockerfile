2ed56430fc8aa886db11b6e8a8f43b49339f6c70
hashito <hasitozzz@gmail.com>
2020-08-12 06:56:52 +0900

    delete comment

--- dynamodb-python3-dql/python3-app.Dockerfile
+++ dynamodb-python3-dql/python3-app.Dockerfile
@@ -6,10 +6,5 @@ RUN pip install dql boto3
 ADD ./db-init.py /root/
 ADD ./sample.py /root/
 ADD ./run.sh /root/
-#RUN /bin/bash -c 'echo "#!/bin/bash" >/root/run.sh' & \
-#    /bin/bash -c 'echo "echo start-python-dynamodb" >>/root/run.sh' & \
-#    /bin/bash -c 'echo "python /root/db-init.py" >>/root/run.sh' & \
-#    /bin/bash -c 'echo "python /root/sample.py" >>/root/run.sh' & \
-#    chmod a+x /root/run.sh
 
 CMD ["/root/run.sh"]
--- dynamodb-python3-dql/sample.py
+++ dynamodb-python3-dql/sample.py
@@ -1,16 +1,11 @@
 import dql
 import boto3
 import os
-#import dynamo3
 table_name=os.environ['TABLE_NAME'] # dynamodb
-#end_point=os.environ['END_POINT']   # http://172.19.0.3:8000
-#dynamodb = boto3.resource('dynamodb',endpoint_url=end_point)
-#table = dynamodb.Table(table_name)
 
 
 engine = dql.Engine()
 c = engine.connect(
-#    session=boto3.Session(),
     region=os.environ['AWS_DEFAULT_REGION'],
     access_key=os.environ['AWS_ACCESS_KEY_ID'],
     secret_key=os.environ['AWS_SECRET_ACCESS_KEY'],
@@ -19,9 +14,6 @@ c = engine.connect(
     is_secure=False
 )
 print("--scan--")
-#    def connect(cls, region, session=None, access_key=None, secret_key=None,
-#                host=None, port=80, is_secure=True, **kwargs):
-#results = engine.execute(f"SELECT * FROM {table_name} LIMIT 10")
 results = engine.execute(f"SCAN * FROM {table_name} LIMIT 10")
 for item in results:
     print(dict(item))
@@ -33,4 +25,3 @@ print("--scan--")
 results = engine.execute(f"SCAN * FROM {table_name} LIMIT 10")
 for item in results:
     print(dict(item))
-
